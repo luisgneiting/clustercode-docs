ba7fce4d120df1973daf45f8ce7f37b1c93b87d4
Chris <github.account@chrigel.net>
2018-10-03 22:39:38 +0200

    Add requirements.txt

--- conf.py
+++ conf.py
@@ -26,7 +26,7 @@ author = 'ccremer'
 # The short X.Y version
 version = '1.3'
 # The full version, including alpha/beta/rc tags
-release = 'release-1.3'
+release = '1.3'
 
 
 # -- General configuration ---------------------------------------------------
